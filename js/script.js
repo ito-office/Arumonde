4b2f8c61bde318a11d21dc7b28dbf56bdff0b138
ito-office <ito.office.it@gmail.com>
2026-04-18 02:27:41 +0900

    20260418_css大まかな作業

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// hamburgerメニュー　
+// **
 const btn = document.getElementById('js-hamburger-btn');
 const menu = document.getElementById('hamburger-menu');
 
@@ -22,4 +24,26 @@ links.forEach(link => {
         menu.classList.remove('is-active');
         document.body.style.overflow = 'auto';
     });
+});
+
+
+// 下部 固定ダウンロードバーの制御
+// **
+window.addEventListener('scroll', function() {
+    const heroSection = document.getElementById('hero');
+    const downloadBar = document.querySelector('.download-container.is-fixed');
+
+    // 1. Heroセクションの底辺が「ページの一番上から何px目にあるか」を取得
+    const heroBottom = heroSection.getBoundingClientRect().bottom + window.pageYOffset;
+
+    // 2. タイミングを調整する数字（オフセット）
+    // この数字を大きくするほど、早いタイミング（上の方）でバーが出てきます
+    const offset = 550; 
+
+    // 3. 判定（Heroの底まであと400pxの地点を過ぎたら表示）
+    if (window.pageYOffset > (heroBottom - offset)) {
+        downloadBar.classList.add('is-show');
+    } else {
+        downloadBar.classList.remove('is-show');
+    }
 });
